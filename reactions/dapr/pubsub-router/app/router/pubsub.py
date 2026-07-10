2167151ef254e4a539a3a087d6f60b13b52f8ede
Jeffrey Zhang <jeffreyjpizhang@gmail.com>
2026-07-10 14:41:58 -0700

    chore: scaffold pubsub router class
    Signed-off-by: Jeffrey Zhang <jeffreyjpizhang@gmail.com>

--- reactions/dapr/pubsub-router/app/router/pubsub.py
+++ reactions/dapr/pubsub-router/app/router/pubsub.py
@@ -13,3 +13,15 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 #
+
+
+class PubSubRouter():
+    """
+    Dispatches Drasi events to Dapr pub/sub topics.
+    """
+
+    def __init__(
+        self,
+        name: str = "dapr-pubsub-router",
+    ) -> None:
+        pass
